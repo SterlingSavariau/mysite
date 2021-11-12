a86ae2c188245984bd79c818e38e7a1f9822dc67
Sterling Savariau <sterlingsavariau@Sterlings-MacBook-Pro.local>
2021-11-12 08:25:33 -0500

    First commit. Created mysite, Installed Requirements, Migrated DB

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-iz*w94-7a2o0(w1&vyvj8!3^xqmvq*u_*&16tlomp$bebjx*dx'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
